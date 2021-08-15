44068f636886b92446317270a6d54c424dfa2cf1
Hamza <hamza.h.hamud@gmail.com>
2021-08-15 21:54:27 +0100

    fix: added start dates to modifiers

--- contracts/sales/FairSale.sol
+++ contracts/sales/FairSale.sol
@@ -32,7 +32,8 @@ contract FairSale {
 
     modifier atStageOrderPlacement() {
         require(
-            block.timestamp < auctionEndDate,
+            block.timestamp < auctionEndDate && 
+            block.timestamp >= auctionStartDate,
             "no longer in order placement phase"
         );
         _;
@@ -40,7 +41,8 @@ contract FairSale {
 
     modifier atStageOrderPlacementAndCancelation() {
         require(
-            block.timestamp < orderCancellationEndDate,
+            block.timestamp < orderCancellationEndDate && 
+            block.timestamp >= auctionStartDate,
             "no longer in order placement and cancelation phase"
         );
         _;
@@ -104,7 +106,7 @@ contract FairSale {
     IERC20 public tokenOut;
     IERC20 public tokenIn;
     uint256 public orderCancellationEndDate;
-    uint256 public auctionStartDate,
+    uint256 public auctionStartDate;
     uint256 public auctionEndDate;
     bytes32 public initialAuctionOrder;
     uint256 public minimumBiddingAmountPerOrder;
@@ -162,10 +164,17 @@ contract FairSale {
             _orderCancellationEndDate <= _auctionEndDate,
             "time periods are not configured correctly"
         );
-        require(_auctionStartDate => block.timestamp, "auction start date cannot be in the past");
+        require(
+            _orderCancellationEndDate > _auctionStartDate,
+            "Cancellation date must be set after the auction start date"
+        );
         require(
             _auctionEndDate > block.timestamp,
             "auction end date must be in the future"
+        );
+        require(
+            _auctionStartDate >= block.timestamp,
+            "auction start date must be set at the present time or in the future"
         );
         sellOrders.initializeEmptyList();
         uint64 userId = getUserId(msg.sender);
@@ -548,7 +557,8 @@ contract FairSale {
             IERC20 _tokenIn,
             IERC20 _tokenOut,
             uint256 _orderCancelationPeriodDuration,
-            uint256 _duration,
+            uint256 _auctionStartDate,
+            uint256 _auctionEndDate,
             uint96 _totalTokenOutAmount,
             uint96 _minBidAmountToReceive,
             uint256 _minimumBiddingAmountPerOrder,
@@ -559,6 +569,7 @@ contract FairSale {
             (
                 IERC20,
                 IERC20,
+                uint256,
                 uint256,
                 uint256,
                 uint96,
@@ -573,7 +584,8 @@ contract FairSale {
             _tokenIn,
             _tokenOut,
             _orderCancelationPeriodDuration,
-            _duration,
+            _auctionStartDate,
+            _auctionEndDate,
             _totalTokenOutAmount,
             _minBidAmountToReceive,
             _minimumBiddingAmountPerOrder,
@@ -616,7 +628,7 @@ contract FairSale {
     }
 
     function getSecondsRemainingInBatch() public view returns (uint256) {
-        if (auctionEndDate < block.timestamp) {
+        if (auctionEndDate < block.timestamp || auctionStartDate > block.timestamp) {
             return 0;
         }
         return auctionEndDate.sub(block.timestamp);
