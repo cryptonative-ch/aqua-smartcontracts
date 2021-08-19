21b8af12fc2ad67ab8676ac2cdec16f0efaff017
Daniel Cybulka <danielcybulka@gmail.com>
2021-08-19 16:43:17 +0200

    fix deployment scripts

--- deploy/aquaSuite/010_register_saleLauncher_templates.ts
+++ deploy/aquaSuite/010_register_saleLauncher_templates.ts
@@ -14,11 +14,32 @@ const deployment: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
         SaleLauncher.address
     );
 
-    await saleLauncherInstance.addTemplate(FairSale.address);
-    await saleLauncherInstance.addTemplate(FixedPriceSale.address);
+    const isFairSaleAdded = (
+        await saleLauncherInstance.getTemplateId(FairSale.address)
+    ).toNumber();
+
+    if (!isFairSaleAdded) {
+        deployments.log(`Adding FairSale to SaleLauncher...`);
+        await saleLauncherInstance.addTemplate(FairSale.address);
+        deployments.log(
+            `FairSale (${FairSale.address}) registered on SaleLauncher(${saleLauncherInstance.address})`
+        );
+    }
+
+    const isFixedPriceSaleAdded = (
+        await saleLauncherInstance.getTemplateId(FixedPriceSale.address)
+    ).toNumber();
+
+    if (!isFixedPriceSaleAdded) {
+        deployments.log(`Adding FixedPriceSale to SaleLauncher...`);
+        await saleLauncherInstance.addTemplate(FixedPriceSale.address);
+        deployments.log(
+            `FixedPriceSale (${FixedPriceSale.address}) registered on SaleLauncher(${saleLauncherInstance.address})`
+        );
+    }
 };
 
-deployment.tags = [TAGS.AQUA];
+deployment.tags = [TAGS.AQUA, TAGS.REGISTER_SALE_TEMPLATES];
 deployment.dependencies = [
     TAGS.SALE_LAUNCHER,
     TAGS.FIXED_PRICE_SALE,
