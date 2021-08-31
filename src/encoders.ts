89c491f20d11d0a4e7b53ea67067966270de6718
Daniel Cybulka <danielcybulka@gmail.com>
2021-08-31 17:33:43 +0200

    update FairSale encoder

--- src/encoders.ts
+++ src/encoders.ts
@@ -1,109 +1,128 @@
-import { utils } from 'ethers'
+import { utils } from "ethers";
 
-import { BigNumber, BigNumberish } from '@ethersproject/bignumber'
+import { BigNumberish } from "@ethersproject/bignumber";
 export interface InitDataFairSaleOptions {
-  saleLauncher: string
-  saleTemplateId: BigNumberish
-  tokenOut: string
-  tokenIn: string
-  auctionEndDate: BigNumberish
-  tokenOutSupply: BigNumberish
-  minPrice: BigNumber
-  minBuyAmount: BigNumber
-  minRaise: BigNumber
-  tokenSupplier: string
+    saleLauncher: string;
+    saleTemplateId: BigNumberish;
+    tokenIn: string;
+    tokenOut: string;
+    duration: number;
+    tokensForSale: BigNumberish;
+    minPrice: BigNumberish;
+    minBuyAmount: BigNumberish;
+    minRaise: BigNumberish;
+    orderCancelationPeriodDuration: number;
+    minimumBiddingAmountPerOrder: BigNumberish;
+    tokenSupplier: string;
 }
 
 export function encodeInitDataFairSale({
-  saleLauncher,
-  saleTemplateId,
-  tokenOut,
-  tokenIn,
-  auctionEndDate,
-  tokenOutSupply,
-  minPrice,
-  minBuyAmount,
-  minRaise,
-  tokenSupplier,
+    saleLauncher,
+    saleTemplateId,
+    tokenIn,
+    tokenOut,
+    duration,
+    tokensForSale,
+    minPrice,
+    minBuyAmount,
+    minRaise,
+    orderCancelationPeriodDuration,
+    minimumBiddingAmountPerOrder,
+    tokenSupplier,
 }: InitDataFairSaleOptions): string {
-  return utils.defaultAbiCoder.encode(
-    ['address', 'uint256', 'address', 'address', 'uint256', 'uint256', 'uint96', 'uint96', 'uint256', 'address'],
-    [
-      saleLauncher,
-      saleTemplateId,
-      tokenOut,
-      tokenIn,
-      auctionEndDate,
-      tokenOutSupply,
-      minPrice,
-      minBuyAmount,
-      minRaise,
-      tokenSupplier,
-    ]
-  )
+    return utils.defaultAbiCoder.encode(
+        [
+            "address",
+            "uint256",
+            "address",
+            "address",
+            "uint256",
+            "uint256",
+            "uint96",
+            "uint96",
+            "uint256",
+            "uint256",
+            "uint256",
+            "address",
+        ],
+        [
+            saleLauncher,
+            saleTemplateId,
+            tokenIn,
+            tokenOut,
+            duration,
+            tokensForSale,
+            minPrice,
+            minBuyAmount,
+            minRaise,
+            orderCancelationPeriodDuration,
+            minimumBiddingAmountPerOrder,
+            tokenSupplier,
+        ]
+    );
 }
 
 export interface InitDataFixedPriceSaleOptions {
-  saleLauncher: string
-  saleTemplateId: BigNumberish
-  tokenSupplier: string
-  tokenOut: string
-  tokenIn: string
-  tokenPrice: BigNumberish
-  tokensForSale: BigNumberish
-  startDate: BigNumberish
-  endDate: BigNumberish
-  minCommitment: BigNumberish
-  maxCommitment: BigNumberish
-  minRaise: BigNumberish
-  participantList: boolean
+    saleLauncher: string;
+    saleTemplateId: BigNumberish;
+    tokenSupplier: string;
+    tokenOut: string;
+    tokenIn: string;
+    tokenPrice: BigNumberish;
+    tokensForSale: BigNumberish;
+    startDate: BigNumberish;
+    endDate: BigNumberish;
+    minCommitment: BigNumberish;
+    maxCommitment: BigNumberish;
+    minRaise: BigNumberish;
+    participantList: boolean;
 }
 
 export function encodeInitDataFixedPriceSale({
-  saleLauncher,
-  saleTemplateId,
-  tokenSupplier,
-  tokenIn,
-  tokenOut,
-  tokenPrice,
-  tokensForSale,
-  startDate,
-  endDate,
-  minCommitment,
-  maxCommitment,
-  minRaise,
-  participantList,
+    saleLauncher,
+    saleTemplateId,
+    tokenSupplier,
+    tokenIn,
+    tokenOut,
+    tokenPrice,
+    tokensForSale,
+    startDate,
+    endDate,
+    minCommitment,
+    maxCommitment,
+    minRaise,
+    participantList,
 }: InitDataFixedPriceSaleOptions): string {
-  return utils.defaultAbiCoder.encode(
-    [
-      'address',
-      'uint256',
-      'address',
-      'address',
-      'address',
-      'uint256',
-      'uint256',
-      'uint256',
-      'uint256',
-      'uint256',
-      'uint256',
-      'uint256',
-      'bool',
-    ],
-    [
-      saleLauncher,
-      saleTemplateId,
-      tokenSupplier,
-      tokenIn,
-      tokenOut,
-      tokenPrice,
-      tokensForSale,
-      startDate,
-      endDate,
-      minCommitment,
-      maxCommitment,
-      minRaise,
-      participantList,
-    ]
-  )
+    return utils.defaultAbiCoder.encode(
+        [
+            "address",
+            "uint256",
+            "address",
+            "address",
+            "address",
+            "uint256",
+            "uint256",
+            "uint256",
+            "uint256",
+            "uint256",
+            "uint256",
+            "uint256",
+            "bool",
+        ],
+        [
+            saleLauncher,
+            saleTemplateId,
+            tokenSupplier,
+            tokenIn,
+            tokenOut,
+            tokenPrice,
+            tokensForSale,
+            startDate,
+            endDate,
+            minCommitment,
+            maxCommitment,
+            minRaise,
+            participantList,
+        ]
+    );
 }
